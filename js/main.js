5efff1b0a52fe9b0fde8020b7a09699373072686
nschuelke1 <ndschuelke@wisc.edu>
2025-03-17 20:50:12 -0500

    CIrcles with City Pop data

--- js/main.js
+++ js/main.js
@@ -30,21 +30,45 @@ window.onload = function(){
                 .attr("y", 50) //position from top on the y (vertical) axis
                 .style("fill", "#FFFFFF"); //fill color
 
-                var dataArray = [10, 20, 30, 40, 50];
-
-                var circles = container.selectAll(".circles") //but wait--there are no circles yet!
-                    .data(dataArray) //here we feed in an array
+                var cityPop = [
+                    { 
+                        city: 'Madison',
+                        population: 233209
+                    },
+                    {
+                        city: 'Milwaukee',
+                        population: 594833
+                    },
+                    {
+                        city: 'Green Bay',
+                        population: 104057
+                    },
+                    {
+                        city: 'Superior',
+                        population: 27244
+                    }
+                ];
+            
+                //Example 2.6 line 3
+                var circles = container.selectAll(".circles") //create an empty selection
+                    .data(cityPop) //here we feed in an array
                     .enter() //one of the great mysteries of the universe
-                    .append("circle") //add a circle for each datum
-                    .attr("class", "circles") //apply a class name to all circles
-                    .attr("r", function(d, i){ //circle radius
-                        console.log("d:", d, "i:", i); //let's take a look at d and i
-                        return d;
+                    .append("circle") //inspect the HTML--holy crap, there's some circles there
+                    .attr("class", "circles")
+                    .attr("id", function(d){
+                        return d.city;
                     })
-                    .attr("cx", function(d, i){ //x coordinate
-                        return 70 + (i * 180);
+                    .attr("r", function(d){
+                        //calculate the radius based on population value as circle area
+                        var area = d.population * 0.01;
+                        return Math.sqrt(area/Math.PI);
                     })
-                    .attr("cy", function(d){ //y coordinate
-                        return 450 - (d * 5);
+                    .attr("cx", function(d, i){
+                        //use the index to place each circle horizontally
+                        return 90 + (i * 180);
+                    })
+                    .attr("cy", function(d){
+                        //subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
+                        return 450 - (d.population * 0.0005);
                     });
 };
