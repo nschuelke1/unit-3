391fbc4fb6d6f0f8b825766e98de615b9da52e6a
nschuelke1 <ndschuelke@wisc.edu>
2025-03-17 20:46:20 -0500

    Added circles

--- js/main.js
+++ js/main.js
@@ -29,4 +29,22 @@ window.onload = function(){
                 .attr("x", 50) //position from left on the x (horizontal) axis
                 .attr("y", 50) //position from top on the y (vertical) axis
                 .style("fill", "#FFFFFF"); //fill color
+
+                var dataArray = [10, 20, 30, 40, 50];
+
+                var circles = container.selectAll(".circles") //but wait--there are no circles yet!
+                    .data(dataArray) //here we feed in an array
+                    .enter() //one of the great mysteries of the universe
+                    .append("circle") //add a circle for each datum
+                    .attr("class", "circles") //apply a class name to all circles
+                    .attr("r", function(d, i){ //circle radius
+                        console.log("d:", d, "i:", i); //let's take a look at d and i
+                        return d;
+                    })
+                    .attr("cx", function(d, i){ //x coordinate
+                        return 70 + (i * 180);
+                    })
+                    .attr("cy", function(d){ //y coordinate
+                        return 450 - (d * 5);
+                    });
 };
